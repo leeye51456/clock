fd07d79a65d0a243be9bf5582c432488440be1d7
leeye51456 <leeye51456@gmail.com>
2021-01-29 01:17:55 +0900

    Add format option to Clock component

--- src/Clock.ts
+++ src/Clock.ts
@@ -1,16 +1,42 @@
 import { format } from 'date-fns';
 import AbstractComponent from './AbstractComponent';
 
+interface OptionalDateTimeFormat {
+  date?: string,
+  time?: string,
+}
+
+interface DateTimeFormat extends OptionalDateTimeFormat {
+  date: string,
+  time: string,
+}
+
+interface ClockOptions {
+  format?: OptionalDateTimeFormat,
+}
+
 class Clock extends AbstractComponent {
   private intervalId: number | null = null;
+
   private timeSection: HTMLElement = document.createElement('section');
   private dateSection: HTMLElement = document.createElement('section');
 
-  constructor() {
+  private format: DateTimeFormat = {
+    date: 'y-MM-dd',
+    time: 'HH:mm:ss',
+  };
+
+  constructor(options?: ClockOptions) {
     super();
 
     this.timeSection.classList.add('clock-time');
     this.dateSection.classList.add('clock-date');
+
+    this.format = {
+      ...this.format,
+      ...options?.format,
+    };
+
     this.update();
   }
 
@@ -30,8 +56,8 @@ class Clock extends AbstractComponent {
 
   private update(): void {
     const date: Date = new Date();
-    this.timeSection.innerText = format(date, 'HH:mm:ss');
-    this.dateSection.innerText = format(date, 'y-MM-dd');
+    this.timeSection.innerText = format(date, this.format.time);
+    this.dateSection.innerText = format(date, this.format.date);
   }
 }
 
