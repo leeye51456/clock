3646c1bf674e5beee8bc1fa1be1dc36f77fb45fd
leeye51456 <leeye51456@gmail.com>
2021-02-12 22:14:21 +0900

    Import `src/swManager.ts` to `src/index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { register } from './swManager';
 import Clock from './Clock';
 import './index.css';
+
+register();
 
 const clock = new Clock();
 clock.draw(document.querySelector('.clock'));
