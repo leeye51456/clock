625da410caa09d45c6d0c9a4ec1e7983fef189ea
leeye51456 <leeye51456@gmail.com>
2021-02-20 03:33:56 +0900

    Add `<datalist>` element and initialize it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import { register } from './swManager';
+import localeKeys, { LocaleKey } from './locale/keys';
+import toBcp47Locale from './toBcp47Locale';
 import Clock from './Clock';
 import './index.css';
 
 register();
+
+const localesDataList: HTMLDataListElement | null = document.querySelector('datalist#locales');
+if (localesDataList) {
+  localeKeys.forEach((locale: LocaleKey) => {
+    const bcp47Locale: string | null = toBcp47Locale(locale);
+    if (bcp47Locale) {
+      const localeOption: HTMLOptionElement = document.createElement('option');
+      localeOption.value = bcp47Locale;
+      localesDataList.appendChild(localeOption);
+    }
+  });
+}
 
 const clock = new Clock();
 clock.draw(document.querySelector('.clock'));
