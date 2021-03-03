1f04998acd1a3dd156aa0142a2e7288cc4f4bf21
leeye51456 <leeye51456@gmail.com>
2021-03-04 01:06:21 +0900

    Eliminate callback hell from `src/db/index.ts`

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,44 +7,50 @@ const databaseName: string = 'clock';
 const storeName: string = 'module';
 let db: IDBDatabase | null = null;
 
-export function initializeFromDatabase(
+function openDatabase(
+  name: string,
+  version: number,
+  onupgradeneeded: (event: IDBVersionChangeEvent) => any,
+): Promise<Event> {
+  return new Promise((resolve, reject) => {
+    const request: IDBOpenDBRequest = window.indexedDB.open(name, version);
+    request.onsuccess = resolve;
+    request.onerror = reject;
+    request.onupgradeneeded = onupgradeneeded;
+  });
+}
+
+export async function initializeFromDatabase(
   loadFromData: (key: number, moduleData: ModuleData) => any,
   defaultOperation: () => any,
-): void {
+): Promise<void> {
   if (!window.indexedDB) {
     defaultOperation();
     return;
   }
 
-  const request: IDBOpenDBRequest = window.indexedDB.open(databaseName, 1);
-
-  request.onsuccess = function (event: Event) {
-    db = (event.target as IDBOpenDBRequest).result;
-
-    const transaction: IDBTransaction = db.transaction(storeName);
-    const store: IDBObjectStore = transaction.objectStore(storeName);
-
-    store.count().onsuccess = function (event: Event) {
-      if ((event.target as IDBRequest).result === 0) {
-        defaultOperation();
-        return;
-      }
-
-      store.openCursor().onsuccess = function (event: Event) {
-        const cursor: IDBCursorWithValue = (event.target as IDBRequest).result;
-        if (cursor) {
-          loadFromData(cursor.key as number, cursor.value);
-          cursor.continue();
-        }
-      };
-    };
-  };
-
-  request.onupgradeneeded = function (event: IDBVersionChangeEvent) {
+  const event: Event = await openDatabase(databaseName, 1, (event: IDBVersionChangeEvent) => {
     const db = (event.target as IDBOpenDBRequest).result;
 
     if (event.oldVersion === 0) {
       db.createObjectStore(storeName, { autoIncrement : true });
+    }
+  });
+
+  db = (event.target as IDBOpenDBRequest).result;
+
+  const tx: IDBTransaction = db.transaction(storeName);
+  const store: IDBObjectStore = tx.objectStore(storeName);
+
+  let count = 0;
+  store.openCursor().onsuccess = (event: Event) => {
+    const cursor: IDBCursorWithValue = (event.target as IDBRequest).result;
+    if (cursor) {
+      count += 1;
+      loadFromData(cursor.key as number, cursor.value);
+      cursor.continue();
+    } else if (count === 0) {
+      defaultOperation();
     }
   };
 };
